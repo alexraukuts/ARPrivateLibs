1c33a723b21644f783b2181e3fa62d16fb567cec
Aliaksandr Raukuts <aliaksandr_raukuts@epam.com>
2016-12-06 13:37:44 +0300

    001 source

---
+++ ARFirstLib.swift
@@ -0,0 +1,15 @@
+//
+//  ARFirstLib.swift
+//  Sample
+//
+//  Created by Aliaksandr Raukuts on 12/2/16.
+//  Copyright © 2016 Aliaksandr Raukuts. All rights reserved.
+//
+
+import Foundation
+
+class ARFirstLib {
+    static func ar1_print(name: String) {
+        print(name)
+    }
+}
